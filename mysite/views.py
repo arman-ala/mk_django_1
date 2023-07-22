0df2e806445369a86788fad80a6df7a36b93baea
Arman Ala <arman.ala.developer@gmail.com>
2023-07-22 10:20:59 +0330

    index, about and contact views are created.

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index_view(request):
+    return HttpResponse("<h1> This is Home Page </h1>")
+
+
+def about_view(request):
+    return HttpResponse("<h1> This is About Page </h1>")
+
+
+def contact_view(request):
+    return HttpResponse("<h1> This is Contact Page </h1>")
