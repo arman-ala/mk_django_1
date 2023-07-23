ede8dd8c260b4357a12e6f575dc60c3c64e98c1d
Arman Ala <arman.ala.developer@gmail.com>
2023-07-23 13:16:27 +0330

    blog.views has 2 new views, blog_single and blog_index.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def blog_view(request):
+    # return render(request)
+    pass
+
+
+def blog_single(request):
+    # return render(request)
+    pass
