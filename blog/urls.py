eae11db0cb5685d42e28d09c50acc652280c767a
Arman Ala <arman.ala.developer@gmail.com>
2023-07-23 13:14:01 +0330

    urls.py has app name and 2 paths.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from blog.views import *
 
+
+app_name = 'blog'
 
 urlpatterns = [
-    path(),
+    path('blog/', blog_view, name='index'),
+    path('blog/single', blog_single, name='single'),
 ]
