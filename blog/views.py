e5de06d93afd1eda3e6322696359f8a295162f96
Arman Ala <arman.ala.developer@gmail.com>
2023-07-23 13:33:18 +0330

    blog.views is now linked to its templates.

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def blog_view(request):
-    # return render(request)
-    pass
+    return render(request, 'blog/blog-home.html')
 
 
 def blog_single(request):
-    # return render(request)
-    pass
+    return render(request, 'blog/blog-single.html')
