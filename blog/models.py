44747e2e71398bd8276faf10ad896824410af6b5
Arman Ala <arman.ala.developer@gmail.com>
2023-07-24 12:35:24 +0330

    blog.Post model got updated and migrations were made.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    # author = models.ForeignKey()
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # image
+    # category = models.ForeignKey()
+    # tags = models.ForeignKey()
+    counted_views = models.PositiveIntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
