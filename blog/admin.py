d6ce55fc610eee2244048e6f5e487656a69752cb
Arman Ala <arman.ala.developer@gmail.com>
2023-07-25 12:01:57 +0330

    date_hierarchy is used to group posts by their created_date field.

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = "created_date"
 
 
 admin.site.register(Post, PostAdmin)
