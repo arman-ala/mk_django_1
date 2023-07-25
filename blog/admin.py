1ab4aa3f407b36e81b6dccd25f7b9ce804571f7f
Arman Ala <arman.ala.developer@gmail.com>
2023-07-25 12:06:57 +0330

    empty_value_display is used to show desired text for null-valued fields.

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     list_display = ('title', 'counted_views', 'status', 'published_date')
+    empty_value_display = '- - empty - -'
 
 
 admin.site.register(Post, PostAdmin)
