5e1cc41bdc64123027ae30607a4e20f76d5e1d43
Arman Ala <arman.ala.developer@gmail.com>
2023-07-25 12:08:31 +0330

    list_filter is used to have some desired filters to group posts.

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     list_display = ('title', 'counted_views', 'status', 'published_date')
     empty_value_display = '- - empty - -'
+    list_filter = ('status', )
 
 
 admin.site.register(Post, PostAdmin)
