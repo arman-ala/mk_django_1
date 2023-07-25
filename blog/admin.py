892c1606bd7bc9549b8f0291b87cb42abf639f53
Arman Ala <arman.ala.developer@gmail.com>
2023-07-25 12:05:37 +0330

    list_display is used to show selected fields of posts.

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from blog.models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
+    list_display = ('title', 'counted_views', 'status', 'published_date')
 
 
 admin.site.register(Post, PostAdmin)
