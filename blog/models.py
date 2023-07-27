0593eb71f251f3295aac6a820ebe997112852536
Arman Ala <arman.ala.developer@gmail.com>
2023-07-27 16:26:23 +0330

    Category class is created at blog/models.py .

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,20 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to ='blog/', default='blog/default.jpg')
-    # category = models.ForeignKey()
+    category = models.ManyToManyField(Category, null=True)
     # tags = models.ForeignKey()
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
