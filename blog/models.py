1e330ac2be2ab2f0461116507c878d163271e198
Arman Ala <arman.ala.developer@gmail.com>
2023-07-27 09:08:48 +0330

    media directory is added to project root in order to use a default image for image field in blog/models.py and the Post model.

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # image
+    image = models.ImageField(upload_to ='blog/', default='blog/default.jpg')
     # category = models.ForeignKey()
     # tags = models.ForeignKey()
     counted_views = models.PositiveIntegerField(default=0)
