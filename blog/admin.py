e974ea192dbbdb0f0a84453d41e15a70aaf25e8f
Arman Ala <arman.ala.developer@gmail.com>
2023-07-27 08:51:09 +0330

    list_display and list_filter are updated in order to have author option.

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,9 @@ from blog.models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
-    list_display = ('title', 'counted_views', 'status', 'published_date')
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'author')
     empty_value_display = '- - empty - -'
-    list_filter = ('status', )
+    list_filter = ('status', 'author')
     # ordering = ['-created_date']
     search_fields = ('title', 'content')
 
