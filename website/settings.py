035fea832cf8de60656941b69f80a33a6b7adea3
Arman Ala <arman.ala.developer@gmail.com>
2023-07-28 07:20:44 +0330

    truncatechars filter is used to summerize content in blog-home.html .

--- website/settings.py
+++ website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from django.core.management.commands.runserver import Command as rs
-rs.default_port = '8080'
+rs.default_port = '8000'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
