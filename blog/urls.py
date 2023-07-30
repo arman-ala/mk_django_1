4175777876b363c6c1cb0ed79ff22e12a88109a3
Arman Ala <arman.ala.developer@gmail.com>
2023-07-30 15:38:56 +0330

    at blog/urls.py a new path is made to group by author.

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single, name='single'),
     path('category/<str:category_name>', blog_view, name='category'),
+    path('author/<str:author_name>', blog_view, name='author'),
 ]
