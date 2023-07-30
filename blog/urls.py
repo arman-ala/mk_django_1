1ab3c86f257e5271ab71a807c6d635d6251d7ee1
Arman Ala <arman.ala.developer@gmail.com>
2023-07-30 16:08:01 +0330

    search path is implemented in urlpatterns in urls.py .

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single, name='single'),
+    path('search/', blog_search, name='search'),
     path('category/<str:category_name>', blog_view, name='category'),
     path('author/<str:author_name>', blog_view, name='author'),
 ]
