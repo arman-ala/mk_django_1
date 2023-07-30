4c880c0164d910c6da85fa8aa3f47509842c02d1
Arman Ala <arman.ala.developer@gmail.com>
2023-07-30 15:57:09 +0330

    blog.views is now updated in order to handle author-based search.

--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Category
 
 
-def blog_view(request, category_name=None):
+def blog_view(request, category_name=None, author_name=None):
     published_posts = Post.objects.filter(status=True)
-    if category_name == None:
+    selected_posts = []
+    if category_name == None and author_name == None:
         context = {
             'published_posts': published_posts,
         }
-    else:
-        selected_posts = []
+    elif author_name == None:
         for post in published_posts:
             for cat in post.category.all():
                 if category_name == cat.name:
                     selected_posts.append(post)
+        context = {
+            'published_posts': selected_posts,
+        }
+    elif category_name == None:
+        selected_posts = published_posts.filter(author__username = author_name)
         context = {
             'published_posts': selected_posts,
         }
