67a14f5dc9d4fdaef72473e58f27ec4c57079de2
Arman Ala <arman.ala.developer@gmail.com>
2023-08-01 20:12:51 +0330

    Latest Posts inclusion tag was show the first posts but it is now working fine and humanize module is added to INSTALLED_APPS and it is used in latest_posts.html .

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.inclusion_tag('blog/blog-latest_posts.html')
 def latest_posts():
-    published_posts = Post.objects.filter(status=1).order_by('published_date')[:2]
+    published_posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
     return {'posts': published_posts}
 
 
