d732b76aa69ea65c448f9a88728dd571a62b2796
Arman Ala <arman.ala.developer@gmail.com>
2023-08-02 19:20:13 +0330

    django sites framework is added to INSTALLED_APPS.

--- website/settings.py
+++ website/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_extensions',
+    'django.contrib.sites',
     
     'mysite.apps.MysiteConfig',
     'blog.apps.BlogConfig',
