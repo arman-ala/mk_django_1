40f864443d745fcec5ad9247ebe468ec96f078dd
Arman Ala <arman.ala.developer@gmail.com>
2023-08-04 13:55:09 +0330

    debug_toolbar is added to INSTALLED_APPS.

--- website/settings.py
+++ website/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'robots',
+    'debug_toolbar',
     
     'mysite.apps.MysiteConfig',
     'blog.apps.BlogConfig',
